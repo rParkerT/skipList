dc7b40514e6f337671f4ed2bf2a100106b882648
rParkerT <vedrana.tokic@gmail.com>
2020-06-08 20:07:31 +0200

    V1
    Adding UT
    Removing namespace
    Code style
    Single responsibility principle

--- main.cpp
+++ main.cpp
@@ -1,30 +1,11 @@
-#include <iostream>
-#include <string>
 #include "menu.h"
 #include "skipList.h"
-#include <thread>
-
-using namespace std;
+#include <iostream>
 
 int main() {
 
-	SkipList<int> sList;
+  SkipList<int> sList;
+  select(sList);
 
-	cout << "Starting..... " << endl;
-
-	chrono::time_point<chrono::system_clock> start, end; 
-
-	start = chrono::system_clock::now(); 
-	for (int i = 0; i < 10000; ++i){
-        sList.insertNode(i, i, true);
-    }
-    end = chrono::system_clock::now(); 
-  
-    chrono::duration<double> elapsed_seconds = end - start; 
-  
-    cout << "Elapsed time for inserting of 10000 elements in skip list: " << elapsed_seconds.count() << " s" << endl; 
-
-	select(sList);
-
-	return 0;
+  return 0;
 }
--- menu.h
+++ menu.h
@@ -1,116 +1,97 @@
 #pragma once
 
-#include <iostream>
-#include <string>
-#include <limits>
 #include "skipList.h"
-
-using namespace std;
+#include <iostream>
+#include <limits>
+#include <string>
 
 // select the actions for skip list :
 // insert new node
 // erase node
 // print all nodes
 // exit selection
-template<typename T>
-void select(SkipList<T>& skipList) noexcept {
+template <typename T> void select(SkipList<T> &skipList) noexcept {
+  char selection;
 
-	char selection;
+  do {
+    std::cout << std::endl;
+    std::cout << "Select action for skip list" << std::endl;
+    std::cout << "To insert element to skip list, enter - I" << std::endl;
+    std::cout << "To erase element in skip list, enter - E" << std::endl;
+    std::cout << "To print elements of skip list, enter - P" << std::endl;
+    std::cout << "To search element of skip list, enter - S" << std::endl;
+    std::cout << "To exit app, enter - Q" << std::endl;
+    std::cout << std::endl;
+    std::cin >> selection;
 
-	do {
-
-	cout << endl;
-	cout << "Select action for skip list" << endl;
-	cout << "To insert element to skip list, enter - I" << endl;
-	cout << "To erase element in skip list, enter - E" << endl;
-	cout << "To print elements of skip list, enter - P" << endl;
-	cout << "To search element of skip list, enter - S" << endl;
-	cout << "To exit app, enter - Q" << endl;
-	cout << endl;
-	cin >> selection;
-
-	if (selection == 'I' || selection == 'i') {
-		cout << "adding element" << endl;
-		insertNode(skipList);
-	}
-	else if (selection == 'E' || selection == 'e') {
-		cout << "erasing element" << endl;
-		eraseNode(skipList);
-	}
-	else if (selection == 'P' || selection == 'p') {
-		cout << "print all elements" << endl;
-		print(skipList);
-	}
-	else if (selection == 'S' || selection == 's') {
-		cout << "search element in skip list" << endl;
-		searchNode(skipList);
-	}
-	else if (selection != 'I' && selection != 'i' && 
-			selection != 'E' && selection != 'e' &&
-			selection != 'P' && selection != 'p' &&
-			selection != 'Q' && selection != 'q' &&
-			selection != 'S' && selection != 's')
-		cout << "unknown input" << endl;
-	} while (selection != 'Q' && selection != 'q');
-		
+    if (selection == 'I' || selection == 'i') {
+      std::cout << "adding element" << std::endl;
+      insertNodeinList(skipList);
+    } else if (selection == 'E' || selection == 'e') {
+      std::cout << "erasing element" << std::endl;
+      eraseNode(skipList);
+    } else if (selection == 'P' || selection == 'p') {
+      std::cout << "print all elements" << std::endl;
+      print(skipList);
+    } else if (selection == 'S' || selection == 's') {
+      std::cout << "search element in skip list" << std::endl;
+      searchNode(skipList);
+    } else if (selection != 'I' && selection != 'i' && selection != 'E' &&
+               selection != 'e' && selection != 'P' && selection != 'p' &&
+               selection != 'Q' && selection != 'q' && selection != 'S' &&
+               selection != 's')
+      std::cout << "unknown input" << std::endl;
+  } while (selection != 'Q' && selection != 'q');
 }
 
 // insert new node in skip list with key and value defined by user
-template<typename T>
-void insertNode(SkipList<T>& skipList) {
-
-	int key = 0;
-	cout << "Add key of new node" << endl;
-    while(!(cin >> key)){
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        cout << "Invalid input of key, try again: ";
-    }
-
-	T value;
-	cout << "Add value of new node" << endl;
-    while(!(cin >> value)){
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        cout << "Invalid input of value, try again: ";
-    }
-
-	skipList.insertNode(key, value, false);
+template <typename T> void insertNodeinList(SkipList<T> &skipList) {
+  T value;
+  std::cout << "Add value of new node" << std::endl;
+  while (!(std::cin >> value)) {
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cout << "Invalid input of value, try again: ";
+  }
+  if (!skipList.insertNode(value))
+    std::cout << "Duplicate, no node added" << std::endl;
+  else
+    std::cout << "Inserted node with value " << value << std::endl;
 }
 
 // erase node in skip list with key defined by user
-template<typename T>
-void eraseNode(SkipList<T>& skipList) {
-
-	int key = 0;
-	cout << "Add key of node that's to be deleted" << endl;
-    while(!(cin >> key)){
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        cout << "Invalid input of key, try again: ";
-    }
-
-	skipList.eraseNode(key);
+template <typename T> void eraseNode(SkipList<T> &skipList) {
+  T value;
+  std::cout << "Add value of node that's to be deleted" << std::endl;
+  while (!(std::cin >> value)) {
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cout << "Invalid input of value, try again: ";
+  }
+  if (!skipList.eraseNode(value))
+    std::cout << "No node in skip list with value " << value << " to erase"
+              << std::endl;
+  else
+    std::cout << "Erased node with value " << value << std::endl;
 }
 
 // print all nodes in skip list
-template<typename T>
-void print(const SkipList<T>& skipList) {
-
-	skipList.print();
+template <typename T> void print(const SkipList<T> &skipList) {
+  skipList.print();
 }
 
 // search node in skip list with key defined by user
-template<typename T>
-void searchNode(SkipList<T>& skipList) {
-
-	int key = 0;
-	cout << "Add key of node that's to be searched" << endl;
-    while(!(cin >> key)){
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        cout << "Invalid input of key, try again: ";
-    }
-
-	skipList.searchNode(key);
+template <typename T> void searchNode(SkipList<T> &skipList) {
+  T value;
+  std::cout << "Add value of node that's to be searched" << std::endl;
+  while (!(std::cin >> value)) {
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cout << "Invalid input of value, try again: ";
+  }
+  if (!skipList.searchNode(value))
+    std::cout << "No node found with value " << value << " in skip list"
+              << std::endl;
+  else
+    std::cout << "Found node with value " << value << std::endl;
 }
